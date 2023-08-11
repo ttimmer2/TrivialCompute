da4983dfe6f24d271049fd4a2088b0f4d87c73e4
Troy Timmerman <troy.timmerman@rtx.com>
2023-08-11 14:39:07 -0500

    added quick test for dice

--- Source/Server/dice.py
+++ Source/Server/dice.py
@@ -9,3 +9,9 @@ class Dice():
         Return a random number between 1 and the number of sides
         """
         return random.randint(1, self.sides)
+
+
+if __name__ == "__main__":
+    d = Dice(6)
+    for i in range(100):
+        print(d.roll())
