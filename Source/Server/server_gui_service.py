0335cd726195f1a6af1f9f5abeebe8240d843706
EllaimA <102774306+EllaimA@users.noreply.github.com>
2023-08-19 16:32:01 -0700

    Update server_gui_service.py

--- Source/Server/server_gui_service.py
+++ Source/Server/server_gui_service.py
@@ -8,6 +8,7 @@ sys.path.append("../protos/")
 
 import logging
 import random
+import sqlite3
 
 import grpc
 from protos import server_gui_pb2
@@ -18,10 +19,24 @@ server_gui_pb2_grpc.ServerGUIServicer
 class ServerGUIServicer_Server(server_gui_pb2_grpc.ServerGUIServicer):
 
     def RollDiceAndGetValidLandingSpots(self, request, context):
-        # TODO enter logic for category creation here
+        def get_possible_positions(current_position):
+            # Define the offsets
+            offsets = [(-1, 0), (0, -1), (1, 0), (0, 1)]
+            # Use a nested list comprehension to get the possible positions
+            possible_positions = [(current_position[0] + offset[0], current_position[1] + offset[1]) for offset in offsets]
+            # Filter out the positions that are not in the POSITIONS list
+            valid_positions = [position for position in possible_positions if position in POSITIONS]
+            # Return the valid positions
+            return valid_positions
+
+            # Use map() to apply the function to each element in POSITIONS
+            possible_positions_map = map(get_possible_positions, POSITIONS)
+            # Convert the map object to a list of lists
+            possible_positions_list = list(possible_positions_map)
+            # Print the possible positions for (4, 4)
+            print(possible_positions_list[POSITIONS.index((4, 4))])
         print(f"Player {request.Player} from location: {request.Location}")
         return server_gui_pb2.ValidSpotMessage(ValidSpot="4,11,23,1")
-
 
     def HandleNewSpot(self, request, context):
         # TODO add logic for question entering here
@@ -29,7 +44,18 @@ class ServerGUIServicer_Server(server_gui_pb2_grpc.ServerGUIServicer):
         return server_gui_pb2.NewSpotActionOrMessage(Player=request.Player,RT=0)  # rt = response type
  
     def GetQuestion(self, request, context):
-        # TODO add db logic for getting question here
+        # Connect to database
+        conn = sqlite3.connect("trivial_compute.db")
+        cursor = conn.cursor()
+        # Execute query to select all questions
+        query = "SELECT * FROM questions"
+        cursor.execute(query)
+        # Fetch all rows as a list of tuples
+        rows = cursor.fetchall()
+        # Select a random row from the list
+        random_row = random.choice(rows)
+        # Get the question from the row
+        question = random_row[0]
         print(f"Player {request.Player} is requesting a question") # don't need to specify their location, because logic part should have it.
         return server_gui_pb2.QuestionGS(CategoryType="Meta trivia", Question=str("What is the most meta aspect of this category?"))
 
